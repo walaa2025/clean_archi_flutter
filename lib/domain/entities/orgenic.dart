49d23827eb29ffdaa740088cd6e9d74a7d5aaee5
walaa <walaa@gmail.com>
2025-11-09 06:42:35 +0300

    clean_archi_flutter Commit

---
+++ lib/domain/entities/orgenic.dart
@@ -0,0 +1,15 @@
+class Orgenic {
+  final String title;
+  final String link;
+  final String? data;
+  final String snippts;
+  final int? position;
+
+  Orgenic({
+    required this.title,
+    required this.link,
+    required this.data,
+    required this.snippts,
+    required this.position,
+  });
+}
